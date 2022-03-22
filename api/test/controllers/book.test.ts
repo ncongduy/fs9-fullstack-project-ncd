ff546d44631e0e2c482d375b2d75d18545795dd3
Nguyen Cong Duy <ncongduy@gmail.com>
2022-03-22 16:34:02 +0700

    feat: write book.test.ts for services and controller

---
+++ api/test/controllers/book.test.ts
@@ -0,0 +1,135 @@
+import request from 'supertest';
+
+import { BookDocument } from '../../src/models/Book';
+import app from '../../src/app';
+import connect, { MongodHelper } from '../db-helper';
+
+const nonExistingBookId = '5e57b77b5744fa0b461c7906';
+
+async function createBook(override?: Partial<BookDocument>) {
+  let book = {
+    title: 'Understanding your heart',
+    publishedYear: 2016,
+    genres: 'Psychology',
+    page: 200,
+    rating: 5,
+    quantity: 20,
+    author: 'Minh Niem',
+    image: 'http://image',
+  };
+
+  if (override) {
+    book = { ...book, ...override };
+  }
+
+  return await request(app).post('/api/v1/books').send(book);
+}
+
+describe('book controller', () => {
+  let mongodHelper: MongodHelper;
+
+  beforeAll(async () => {
+    mongodHelper = await connect();
+  });
+
+  afterEach(async () => {
+    await mongodHelper.clearDatabase();
+  });
+
+  afterAll(async () => {
+    await mongodHelper.closeDatabase();
+  });
+
+  it('should create a book', async () => {
+    const res = await createBook();
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('_id');
+    expect(res.body.title).toBe('Understanding your heart');
+  });
+
+  it('should not create a book with wrong data', async () => {
+    const res = await request(app).post('/api/v1/books').send({
+      title: 'Understanding your heart',
+      publishedYear: 2016,
+      genres: 'Psychology',
+      page: 200,
+      rating: 5,
+      // quantity: 20,
+      // author: 'Minh Niem',
+      // image: 'http://image',
+    });
+    expect(res.status).toBe(400);
+  });
+
+  it('should get back an existing book', async () => {
+    let res = await createBook();
+    expect(res.status).toBe(200);
+
+    const bookId = res.body._id;
+    res = await request(app).get(`/api/v1/books/${bookId}`);
+
+    expect(res.body._id).toEqual(bookId);
+  });
+
+  it('should not get back a non-existing movie', async () => {
+    const res = await request(app).get(`/api/v1/books/${nonExistingBookId}`);
+    expect(res.status).toBe(404);
+  });
+
+  it('should get back all book', async () => {
+    const res1 = await createBook({
+      title: 'Understanding your heart',
+      publishedYear: 2016,
+      page: 200,
+      rating: 5,
+      quantity: 20,
+      author: 'Minh Niem',
+      image: 'http://image',
+    });
+    const res2 = await createBook({
+      title: 'Work as a journey',
+      publishedYear: 2016,
+      page: 200,
+      rating: 5,
+      quantity: 20,
+      author: 'Minh Niem',
+      image: 'http://image',
+    });
+
+    const res3 = await request(app).get('/api/v1/books');
+
+    expect(res3.body.length).toEqual(2);
+    expect(res3.body[0]._id).toEqual(res1.body._id);
+    expect(res3.body[1]._id).toEqual(res2.body._id);
+  });
+
+  it('should update an existing movie', async () => {
+    let res = await createBook();
+    expect(res.status).toBe(200);
+
+    const bookId = res.body._id;
+    const update = {
+      title: 'Love myself',
+      publishedYear: 2018,
+    };
+
+    res = await request(app).put(`/api/v1/books/${bookId}`).send(update);
+
+    expect(res.status).toEqual(200);
+    expect(res.body.title).toEqual('Love myself');
+    expect(res.body.publishedYear).toEqual(2018);
+  });
+
+  it('should delete an existing movie', async () => {
+    let res = await createBook();
+    expect(res.status).toBe(200);
+    const bookId = res.body._id;
+
+    res = await request(app).delete(`/api/v1/books/${bookId}`);
+
+    expect(res.status).toEqual(204);
+
+    res = await request(app).get(`/api/v1/books/${bookId}`);
+    expect(res.status).toBe(404);
+  });
+});
---
+++ api/test/services/book.test.ts
@@ -0,0 +1,103 @@
+import Book from '../../src/models/Book';
+import BookServices from '../../src/services/book';
+import connect, { MongodHelper } from '../db-helper';
+
+const nonExistingBookId = '5e57b77b5744fa0b461c7906';
+
+async function createBook() {
+  const movie = new Book({
+    title: 'Understanding your heart',
+    publishedYear: 2016,
+    genres: 'Psychology',
+    page: 200,
+    rating: 5,
+    quantity: 20,
+    author: 'Minh Niem',
+    image: 'http://image',
+  });
+  return await BookServices.create(movie);
+}
+
+describe('book service', () => {
+  let mongodHelper: MongodHelper;
+
+  beforeAll(async () => {
+    mongodHelper = await connect();
+  });
+
+  afterEach(async () => {
+    await mongodHelper.clearDatabase();
+  });
+
+  afterAll(async () => {
+    await mongodHelper.closeDatabase();
+  });
+
+  it('should create a book', async () => {
+    const book = await createBook();
+
+    expect(book).toHaveProperty('_id');
+    expect(book).toHaveProperty('title', 'Understanding your heart');
+    expect(book).toHaveProperty('publishedYear', 2016);
+    expect(book).toHaveProperty('genres', 'Psychology');
+    expect(book).toHaveProperty('page', 200);
+    expect(book).toHaveProperty('rating', 5);
+    expect(book).toHaveProperty('quantity', 20);
+    expect(book).toHaveProperty('author', 'Minh Niem');
+    expect(book).toHaveProperty('image', 'http://image');
+  });
+
+  it('should get a book with id', async () => {
+    const book = await createBook();
+    const found = await BookServices.findById(book._id);
+
+    expect(found.title).toEqual(book.title);
+    expect(found._id).toEqual(book._id);
+  });
+
+  // Check https://jestjs.io/docs/en/asynchronous for more info about
+  // how to test async code, especially with error
+  it('should not get a non-existing movie', async () => {
+    expect.assertions(1);
+
+    return BookServices.findById(nonExistingBookId).catch((e) => {
+      expect(e.message).toMatch(`Book ${nonExistingBookId} not found`);
+    });
+  });
+
+  it('should update an existing book', async () => {
+    const book = await createBook();
+    const update = {
+      title: 'Work as a journey',
+      publishedYear: 2018,
+    };
+    const updated = await BookServices.update(book._id, update);
+
+    expect(updated).toHaveProperty('_id', book._id);
+    expect(updated).toHaveProperty('title', 'Work as a journey');
+    expect(updated).toHaveProperty('publishedYear', 2018);
+  });
+
+  it('should not update a non-existing movie', async () => {
+    expect.assertions(1);
+
+    const update = {
+      title: 'Work as a journey',
+      publishedYear: 2018,
+    };
+
+    return BookServices.update(nonExistingBookId, update).catch((e) => {
+      expect(e.message).toMatch(`Book ${nonExistingBookId} not found`);
+    });
+  });
+
+  it('should delete an existing movie', async () => {
+    expect.assertions(1);
+    const book = await createBook();
+
+    await BookServices.deleteBook(book._id);
+    return BookServices.findById(book._id).catch((e) => {
+      expect(e.message).toBe(`Book ${book._id} not found`);
+    });
+  });
+});
