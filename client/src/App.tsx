934556e9a3ec8f2d4d4fcc77793f5a6f4dccfccb
Nguyen Cong Duy <ncongduy@gmail.com>
2022-03-25 19:54:41 +0700

    feat: set up Topbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Topbar from './components/Topbar';
 import HomePage from './pages/HomePage';
-import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/admin" element={<AdminPage />} />
-    </Routes>
+    <>
+      <Topbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
