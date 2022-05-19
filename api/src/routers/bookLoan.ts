b9cd4fb7862cc25371040302339787aecfbf92f7
Nguyen Cong Duy <ncongduy@gmail.com>
2022-05-19 23:48:16 +0300

    feat: create authorization in bookloan router

--- api/src/routers/bookLoan.ts
+++ api/src/routers/bookLoan.ts
@@ -6,10 +6,11 @@ import { findAll, findById, deleteBookLoan, createBookLoan } from '../controller
 const router = express.Router()
 
 // Every path we define here will get /api/v1/bookloans prefix
-router.get('/', passport.authenticate('jwt', { session: false }), findAll)
-router.get('/:id', findById)
-// router.put('/:id', updateBookLoan)
-router.delete('/:id', deleteBookLoan)
+router.use(['/', '/:slug'], passport.authenticate('jwt', { session: false }))
 router.post('/', createBookLoan)
+router.delete('/:id', deleteBookLoan)
+router.get('/', findAll)
+// router.get('/:id', findById)
+// router.put('/:id', updateBookLoan)
 
 export default router
