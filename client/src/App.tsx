be2f97fb174ed309a1fece0766ac8cb1253cfed0
Nguyen Cong Duy <ncongduy@gmail.com>
2022-05-22 17:53:09 +0300

    feat: setup GlobalStyles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-import GoogleLogin from 'react-google-login'
 
 import HomePage from './pages/HomePage'
 import AdminPage from './pages/AdminPage'
-import axiosClient from './fetchApi/axiosClient'
-
-type ResTypes = {
-  token: string
-  user: {
-    firstName: string
-    lastName: string
-    email: string
-    role: string
-    updateAt: Date
-    createAt: Date
-  }
-}
 
 function App() {
-  const responseGoogle = async (response: any) => {
-    const tokenId = response?.tokenId
-    const res: ResTypes = await axiosClient.post('/users/google-login', {
-      id_token: tokenId,
-    })
-
-    const { user, token } = res
-    localStorage.setItem('access_token', token)
-  }
-
   return (
-    <>
-      <GoogleLogin
-        clientId="628027389160-6mlqifinuua7mrgeml4qa0g9g4j2sbe2.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
   )
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
+import GlobalStyles from './components/GlobalStyles'
 import reportWebVitals from './reportWebVitals'
-import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GlobalStyles>
+        <App />
+      </GlobalStyles>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
